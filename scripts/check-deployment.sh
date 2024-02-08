33d05a188e913b1fa107772c30793cd6fd255231
Dennis <dennisdiepolder@outlook.com>
2024-02-08 20:23:46 +0100

    .

---
+++ scripts/check-deployment.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Starte den Check-Deployment-Skript..."
+
+exec > >(tee -a /var/log/custom/scriptoutputs.log) 2>&1
+
+# Warte, bis Port 80 verfügbar ist
+while ! nc -z localhost 80; do   
+  sleep 1 # Warte für 1 Sekunde vor dem nächsten Versuch
+done
+
+# Führe den curl-Befehl aus und überprüfe den Statuscode
+if curl -sSf http://localhost:80 >/dev/null; then
+  echo "Erfolgreich: Curl-Befehl wurde erfolgreich ausgeführt."
+else
+  echo "Fehler: Curl-Befehl fehlgeschlagen."
+fi
