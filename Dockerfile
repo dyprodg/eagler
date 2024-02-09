2da100b1cd604f8c9007e415ea0ada9ea554af4b
Dennis <dennisdiepolder@outlook.com>
2024-02-09 10:59:21 +0100

    possible fix for sharp, but not shure

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,13 @@
-# Base image
 FROM node:18-alpine AS base
 
 # Install dependencies only when needed
 FROM base AS deps
 RUN apk add --no-cache libc6-compat
 WORKDIR /app
-COPY package.json yarn.lock* package-lock.json* pnpm-lock.yaml* ./
-RUN \
-  if [ -f yarn.lock ]; then yarn --frozen-lockfile; \
-  elif [ -f package-lock.json ]; then npm ci; \
-  elif [ -f pnpm-lock.yaml ]; then corepack enable pnpm && pnpm i --frozen-lockfile; \
-  else echo "Lockfile not found." && exit 1; \
-  fi
+
+COPY package.json package-lock.json* ./
+RUN npm ci
+RUN npm install -g --arch=x64 --platform=linux --libc=glibc sharp@0.33.0-rc.2
 
 # Generate Prisma client
 FROM base AS prisma-generator
@@ -37,6 +33,9 @@ WORKDIR /app
 ENV NODE_ENV production
 # Uncomment if you want to disable telemetry
 # ENV NEXT_TELEMETRY_DISABLED 1
+
+COPY --from=deps --chown=nextjs:nodejs /app/node_modules/sharp ./node_modules/sharp
+ENV NEXT_SHARP_PATH=/app/node_modules/sharp
 
 RUN addgroup --system --gid 1001 nodejs
 RUN adduser --system --uid 1001 nextjs
--- scripts/local-docker-test.sh
+++ scripts/local-docker-test.sh
@@ -1,26 +1,3 @@
-#!/bin/bash
-
-exec > >(tee -a /var/log/custom/scriptoutputs.log) 2>&1
-
-echo "Starting the start.sh script..."
-echo "Starting the start.sh script..."
-echo "Starting the start.sh script..."
-echo "Starting the start.sh script..."
-echo "Starting the start.sh script..."
-echo "Starting the start.sh script..."
-
-# Clean Up
-
-docker container prune -f
-docker image prune -f
-
-# Login to ECR
-aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 283919506801.dkr.ecr.eu-central-1.amazonaws.com
-
-# Pull the Docker image from ECR
-docker pull 283919506801.dkr.ecr.eu-central-1.amazonaws.com/eagler-docker:latest
-
-# Retrieve secrets from AWS Secrets Manager
 SECRET_ARN="arn:aws:secretsmanager:eu-central-1:283919506801:secret:eagler-secrets-A8629Z"
 SECRETS=$(aws secretsmanager get-secret-value --secret-id ${SECRET_ARN} --query SecretString --output text)
 
@@ -37,6 +14,7 @@ AWS_STORAGE_BUCKET_NAME=$(echo $SECRETS | jq -r .AWS_STORAGE_BUCKET_NAME)
 IMAGE_UPLOAD_URL="http://${AWS_BUCKET_NAME}.s3.${AWS_BUCKET_REGION}.amazonaws.com/"
 IMAGE_STORAGE_URL="http://${AWS_STORAGE_BUCKET_NAME}.s3.${AWS_BUCKET_REGION}.amazonaws.com/"
 
+docker build -t eagler-docker-local .
 # Start the Docker container with all the environment variables
 docker run \
   -e DATABASE_URL="$DATABASE_URL" \
@@ -48,14 +26,12 @@ docker run \
   -e AWS_STORAGE_BUCKET_NAME="$AWS_STORAGE_BUCKET_NAME" \
   -e IMAGE_UPLOAD_URL="$IMAGE_UPLOAD_URL" \
   -e IMAGE_STORAGE_URL="$IMAGE_STORAGE_URL" \
-  -e NEXTAUTH_URL="https://www.justanothersocialmedia.net/" \
+  -e NEXTAUTH_URL="http://localhost:3000" \
+  -e NEXT_SHARP_PATH=/tmp/node_modules/sharp \
   -p 80:3000 \
-  --log-driver=awslogs \
-  --log-opt awslogs-region=eu-central-1 \
-  --log-opt awslogs-group=eagler/ec2 \
-  --log-opt awslogs-stream={instance_id} \
   -d \
-  283919506801.dkr.ecr.eu-central-1.amazonaws.com/eagler-docker:latest
+    eagler-docker-local:latest
+
+unset DATABASE_URL NEXTAUTH_SECRET AWS_ACCESS_KEY AWS_SECRET_ACCESS_KEY AWS_BUCKET_NAME AWS_BUCKET_REGION AWS_STORAGE_BUCKET_NAME IMAGE_UPLOAD_URL IMAGE_STORAGE_URL SECRET_ARN SECRETS
 
 
-sleep 10
