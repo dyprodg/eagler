6efbe9c6ee424b455ccb0003bae41cd91e1b4a40
Dennis <dennisdiepolder@outlook.com>
2024-03-18 13:06:31 +0100

    added:new tests

--- _tests_/actions.test.js
+++ _tests_/actions.test.js
@@ -5,6 +5,7 @@ import {
   getLatestPosts,
   getUserPosts,
   deletePost,
+  setLike,
 } from "@/app/actions";
 import prisma from "@/lib/prisma";
 import {
@@ -17,12 +18,22 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 jest.mock("@/lib/prisma", () => ({
   user: {
     delete: jest.fn(),
+    findUnique: jest.fn(),
+  },
+  userAuth: {
+    findUnique: jest.fn(),
+    delete: jest.fn(),
   },
   post: {
     create: jest.fn(),
     findMany: jest.fn(),
     findUnique: jest.fn(),
     delete: jest.fn(),
+  },
+  like: {
+    findFirst: jest.fn(),
+    delete: jest.fn(),
+    create: jest.fn(),
   },
 }));
 
@@ -41,14 +52,6 @@ describe("actions", () => {
     jest.clearAllMocks();
   });
 
-  it("should delete account", async () => {
-    const formData = new Map();
-    formData.set("email", "test@example.com");
-    await deleteAccount(null, formData);
-    expect(prisma.user.delete).toHaveBeenCalledWith({
-      where: { email: "test@example.com" },
-    });
-  });
 
   it("should get signed URL", async () => {
     const session = { user: { id: "1" } };
@@ -80,4 +83,81 @@ describe("actions", () => {
     await deletePost(session, post);
     expect(prisma.post.delete).toHaveBeenCalledWith({ where: { id: "1" } });
   });
+
+  it("should delete the user if they exist", async () => {
+    const prevState = {};
+    const formData = new Map();
+    formData.set('email', 'test@example.com');
+
+    prisma.user.findUnique = jest.fn().mockResolvedValue({ id: 1, email: 'test@example.com' });
+    prisma.userAuth.findUnique = jest.fn().mockResolvedValue({ id: 1, userId: 1 });
+    prisma.user.delete = jest.fn().mockResolvedValue(true);
+    prisma.userAuth.delete = jest.fn().mockResolvedValue(true);
+
+    const result = await deleteAccount(prevState, formData);
+
+    expect(result).toEqual({ message: 'success' });
+    expect(prisma.user.findUnique).toHaveBeenCalledWith({ where: { email: 'test@example.com' } });
+    expect(prisma.userAuth.findUnique).toHaveBeenCalledWith({ where: { userId: 1 } });
+    expect(prisma.user.delete).toHaveBeenCalledWith({ where: { email: 'test@example.com' } });
+    expect(prisma.userAuth.delete).toHaveBeenCalledWith({ where: { id: 1 } });
+  });
+
+  it("should return an error message if the deletion fails", async () => {
+    const prevState = {};
+    const formData = new Map();
+    formData.set('email', 'test@example.com');
+
+    prisma.user.findUnique = jest.fn().mockResolvedValue({ id: 1, email: 'test@example.com' });
+    prisma.user.delete = jest.fn().mockRejectedValue(new Error('Deletion failed'));
+
+    const result = await deleteAccount(prevState, formData);
+
+    expect(result).toEqual({ message: 'User could not be deleted, Error: Deletion failed' });
+  });
+
+  it("should set like for a post", async () => {
+    const session = { user: { id: "1" } };
+    const post = { id: "1" };
+  
+    // Test case when like exists
+    prisma.like.findFirst = jest.fn().mockResolvedValue({ id: 1 });
+    prisma.like.delete = jest.fn().mockResolvedValue(true);
+  
+    let result = await setLike(session, post);
+  
+    expect(result).toEqual({ success: "Like removed" });
+    expect(prisma.like.findFirst).toHaveBeenCalledWith({
+      where: {
+        userId: session.user.id,
+        postId: post.id,
+      },
+    });
+    expect(prisma.like.delete).toHaveBeenCalledWith({
+      where: {
+        id: 1,
+      },
+    });
+  
+    // Test case when like does not exist
+    prisma.like.findFirst = jest.fn().mockResolvedValue(null);
+    prisma.like.create = jest.fn().mockResolvedValue(true);
+  
+    result = await setLike(session, post);
+  
+    expect(result).toEqual({ success: "Like added" });
+    expect(prisma.like.findFirst).toHaveBeenCalledWith({
+      where: {
+        userId: session.user.id,
+        postId: post.id,
+      },
+    });
+    expect(prisma.like.create).toHaveBeenCalledWith({
+      data: {
+        userId: session.user.id,
+        postId: post.id,
+      },
+    });
+  });
+
 });
