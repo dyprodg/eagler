f8c651d7f595b1d5cb4fa596c76926c72d5747b3
Dennis <dennisdiepolder@outlook.com>
2024-03-19 09:51:16 +0100

    new release

--- src/app/(Contentpages)/impressum/page.jsx
+++ src/app/(Contentpages)/impressum/page.jsx
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <div className="flex w-full h-screen justify-center items-start">
         <h1 className="text-5xl">Impressum</h1>
-        <div className="text-2xl">hier soll mal ein impressum hin</div>
+        <div className="text-2xl"></div>
     </div>
   )
 }
