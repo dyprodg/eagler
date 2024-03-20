25699d8bae3b1053239507fc535d08002066b170
Dennis <dennisdiepolder@outlook.com>
2024-03-20 20:47:27 +0100

    fixed: impressum

--- src/app/(Contentpages)/impressum/page.jsx
+++ src/app/(Contentpages)/impressum/page.jsx
@@ -1,19 +1,7 @@
-'use client'
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
 
 const page = () => {
-  const router = useRouter();
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      router.push(process.env.NEXTAUTH_URL);
-    }, 3000);
-
-    // Cleanup function to clear the timer if the component unmounts before the redirect happens
-    return () => clearTimeout(timer);
-  }, [router]);
 
   return (
     <div className="flex w-full h-screen justify-center items-start">
--- src/app/(Fallbackpages)/email-verified/page.jsx
+++ src/app/(Fallbackpages)/email-verified/page.jsx
@@ -1,7 +1,21 @@
+'use client'
 import CheckMark from '@/app/components/loaders/checkMark';
 import React from 'react';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 const EmailVerifiedPage = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        router.push(process.env.NEXTAUTH_URL);
+      }, 3000);
+  
+      // Cleanup function to clear the timer if the component unmounts before the redirect happens
+      return () => clearTimeout(timer);
+    }, [router]);
+  
     return (
         <div className='w-full h-screen flex flex-col justify-center items-center'>
             <h1 className='text-3xl text-shadow'>Email Verified</h1>
